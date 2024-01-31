a02b6562e1848ef240b669fd37b37e054a0a48cd
FoxFate <72974391+pustelnik17@users.noreply.github.com>
2024-01-31 09:45:23 +0100

    Listy nie na ocenę

---
+++ l14.py
@@ -0,0 +1,11 @@
+# Hubert Jackowski
+# Zadanie 1
+
+# import base64
+# value = "Programowanie laczy"
+# value = bytes(value, 'utf-8')
+# encodedValue = base64.b64encode(b"Programowanie laczy")
+# print(base64.b64decode(encodedValue))
+
+# Zadanie 2
+
